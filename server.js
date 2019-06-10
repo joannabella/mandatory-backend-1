d049913b4ae8aff524e373d120ee081ffc63f44b
JoannaAbella <Joanna.Abella96@gmail.com>
2019-06-10 22:15:09 +0200

    Implemented backend and most likely done with frontend soon as well

--- server.js
+++ server.js
@@ -11,19 +11,28 @@ app.use(express.json());
 
 const STORAGE_ROOT = __dirname + '/rooms';
 
+const SOCKET_EVENT = {
+    SEND_MESSAGE: 'send_message',
+    NEW_MESSAGE: 'new_message',
+};
+
 function createRoom(name) {
     try {
         fs.mkdirSync(STORAGE_ROOT + '/' + name);
+        //fs.writeFil
     }
     catch(error) {
 
     }
 }
 
-function createMessage(room, author, body) {
+function createMessage(room, username, content) {
     const timestamp = (+ new Date());
-    const fileContent = JSON.stringify({'author': author, 'body': body});
+    const message = {'username': username, 'timestamp': timestamp, 'content': content};
+    const fileContent = JSON.stringify(message);
     fs.writeFileSync(STORAGE_ROOT + '/' + room + '/' + timestamp + '.json', fileContent);
+    io.emit(SOCKET_EVENT.NEW_MESSAGE, message);
+    console.log(message.username);
 }
 
 app.get('/', function(req, res) {
@@ -58,11 +67,11 @@ app.post('/rooms', function(req, res) {
 });
 
 app.post('/rooms/:name/message', function(req, res) {
-    if (!req.body.author || !req.body.body) {
+    if (!req.body.username || !req.body.content) {
         res.status(400).end();
         return;
     }
-    createMessage(req.params.name, req.body.author, req.body.body);
+    createMessage(req.params.name, req.body.username, req.body.content);
     res.status(200).end();
 });
 
@@ -73,9 +82,11 @@ app.delete('/rooms/:name', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-    socket.on('chat message', function(message) {
-        console.log('message: ' + message);
+    socket.on(SOCKET_EVENT.SEND_MESSAGE, function(message) {
+        console.log(message);
+        createMessage(message.room, message.username, message.content);
     });
+    
 });
 
 try {
@@ -89,6 +100,6 @@ createRoom('general');
 createRoom('random');
 
 
-http.listen(8000, function() {
+http.listen(8000, function() {  
     console.log('Listening on *:8000');
 });
